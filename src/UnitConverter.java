06a3e11d5354a696aff8c5e7567a50c9ffd2f68c
rumeethundal <rumeethundal03@gmail.com>
2024-01-05 21:09:53 -0800

    created methods for last two cases

--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class UnitConverter {
     static Scanner kb = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int object;
-        while(true) {
+        while (true) {
             System.out.println("\nWelcome to the Unit Converter\n" +
                     "Take your pick!\n" +
                     "Choose 1: To convert from kilometers to miles\n" +
@@ -16,7 +16,8 @@ public class UnitConverter {
 
             object = kb.nextInt();
 
-            switch(object) {
+
+            switch (object) {
                 case 1:
                     convertKilometersToMiles();
                     break;
@@ -24,23 +25,59 @@ public class UnitConverter {
                 case 2:
                     convertKilogramsToPounds();
                     break;
+
+                case 3:
+                    convertCelsiusToFahrenheit();
+                    break;
+
+                case 4:
+                    convertCentimetersToInches();
+                    break;
+
+                case 5:
+                    convertMetersToYards();
+                    break;
+
+//                default:
+//                    System.out.print("Invalid: ");
             }
         }
     }
-     public static void convertKilometersToMiles() {
+
+    public static void convertKilometersToMiles() {
         System.out.println("Enters kilometers to convert to miles:");
         double kilometers = kb.nextDouble();
         //System.out.println("Enters kilometers to convert to miles:");
         double miles = kilometers * 0.62;
         System.out.println(kilometers + " kilometers is equal to " + miles + " miles");
     }
+
     public static void convertKilogramsToPounds() {
         System.out.println("Enter kilograms to convert to pounds:");
         double kilograms = kb.nextDouble();
         double pounds = kilograms * 2.2;
         System.out.print(kilograms + " kilograms is equal to " + pounds + " pounds");
     }
+
     public static void convertCelsiusToFahrenheit() {
+        System.out.println("Enter celsius to convert to fahrenheit:");
+        double celsius = kb.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println(celsius+" celsius is equal to "+fahrenheit +" fahrenheit");
+    }
+
+    public static void convertCentimetersToInches() {
+        System.out.println("Enter centimeters to convert to inches");
+        double centimeters = kb.nextDouble();
+        double inches = centimeters * 0.39;
+        System.out.println(centimeters + "centimeters is equal to " + inches + "inches");
+    }
+
+    public static void convertMetersToYards(){
+        System.out.println("Enter meters to convert to yards:");
+        double meters = kb.nextDouble();
+        double yards = meters * 1.0936;
+        System.out.println(meters + " meters is equal to " + yards + " yards");
     }
 
 }
